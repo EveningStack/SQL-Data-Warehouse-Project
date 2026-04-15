262767d7c4944686e9d3b289e090247f8b87e76a
Andrey O <oleskyinbox@gmail.com>
2026-04-15 19:05:49 -0400

    Checked crm_cust_info for NULLs and extra spaces.

---
+++ scripts/silver/data_correctness_checks.sql
@@ -0,0 +1,63 @@
+-- Check for NULLs or duplicates in Primary Key
+-- Expectation: No result
+
+SELECT
+	cst_id,
+	COUNT(*)
+FROM bronze.crm_cust_info
+GROUP BY cst_id
+HAVING COUNT(*) > 1 OR cst_id IS NULL;
+
+SELECT
+	*,
+	ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+FROM bronze.crm_cust_info
+WHERE cst_id = 29466;
+
+SELECT
+	*
+FROM (
+	SELECT
+		*,
+		ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+	FROM bronze.crm_cust_info) t WHERE t.flag_last != 1;
+
+SELECT
+	*
+FROM (
+	SELECT
+		*,
+		ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+	FROM bronze.crm_cust_info) t WHERE t.flag_last = 1;
+
+-- Check for unwanted spaces
+-- Expectation: No result
+
+SELECT
+	cst_firstname
+FROM bronze.crm_cust_info
+WHERE cst_firstname != TRIM(cst_firstname);
+
+SELECT
+	cst_lastname
+FROM bronze.crm_cust_info
+WHERE cst_lastname != TRIM(cst_lastname);
+
+SELECT
+	cst_gndr
+FROM bronze.crm_cust_info
+WHERE cst_gndr != TRIM(cst_gndr);
+
+SELECT
+	cst_id,
+	cst_key,
+	TRIM(cst_firstname) AS cst_firstname,
+	TRIM(cst_lastname) AS cst_lastname,
+	TRIM(cst_marital_status) AS cst_marital_status,
+	TRIM(cst_gndr) AS cst_gndr,
+	cst_create_date
+FROM (
+	SELECT
+		*,
+		ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+	FROM bronze.crm_cust_info) t WHERE t.flag_last = 1;
